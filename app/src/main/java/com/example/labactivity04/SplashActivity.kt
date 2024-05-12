f42bfa02e4eacbfcaf016bb560fceeb5fbd8d42d
IT22358684 <it22358684@my.sliit.lk>
2024-05-12 19:07:25 +0530

    edit splash activity

--- app/src/main/java/com/example/labactivity04/SplashActivity.kt
+++ app/src/main/java/com/example/labactivity04/SplashActivity.kt
@@ -15,6 +15,6 @@ class SplashActivity : AppCompatActivity() {
         Handler().postDelayed({
             startActivity(i)
             finish()
-            },5000)
+            },1000)
         }
 }
